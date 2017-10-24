159b1494a0391402b002207429aa42233cb4f443
LZ-PC\lz <waly1029@126.com>
2017-10-25 01:40:43 +0900

    update

--- DDGame/Assets/Scripts/UI/PauseMenu.cs
+++ DDGame/Assets/Scripts/UI/PauseMenu.cs
@@ -22,6 +22,7 @@ public class PauseMenu : MonoBehaviour {
 		pauseMenuCanvas = transform.Find ( "Canvas" ).gameObject;
 
 		ps = FindObjectOfType<PauseMenuController> ( );
+
 	}
 
     void Update( ) {
